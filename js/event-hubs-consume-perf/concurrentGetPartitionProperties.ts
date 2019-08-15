492df6a017a4aaf33149b340429de6aa58022823
Mike Harder <mharder@microsoft.com>
2019-08-14 17:13:09 -0700

    Log timestamps

--- js/event-hubs-consume-perf/concurrentGetPartitionProperties.ts
+++ js/event-hubs-consume-perf/concurrentGetPartitionProperties.ts
@@ -13,16 +13,22 @@ async function main(): Promise<void> {
     const partitionIds = await client.getPartitionIds();
     console.log(partitionIds);
 
+    // Takes about 100 ms
+    console.log(`${new Date().toISOString()} Before sequential`);
     const partitionsSequential = [
       await client.getPartitionProperties(partitionIds[0]),
       await client.getPartitionProperties(partitionIds[1])
     ];
+    console.log(`${new Date().toISOString()} After sequential`);
     console.log(partitionsSequential);
 
+    // Takes exactly 90 seconds, which seems too close to a round number to be a coincidence.  Maybe corresponds to an internal timeout?
+    console.log(`${new Date().toISOString()} Before parallel`);
     const partitionsParallel = await Promise.all([
       client.getPartitionProperties(partitionIds[2]),
       client.getPartitionProperties(partitionIds[3]),
     ]);
+    console.log(`${new Date().toISOString()} After paralllel`);
     console.log(partitionsParallel);
   }
   finally {
