0538dd52cc708ece6315fb5fd13ea40e547caf9a
Mike Harder <mharder@microsoft.com>
2019-08-16 12:01:18 -0700

    Print partitons

--- python/recv_async.py
+++ python/recv_async.py
@@ -18,11 +18,54 @@ BYTES_PER_MESSAGE = 1024
 
 CONNECTION_STRING = os.environ.get("EVENT_HUBS_CONNECTION_STRING")
 
-clients = []
-for i in range(args.clients):
-    clients.append(EventHubClient.from_connection_string(CONNECTION_STRING, event_hub_path=EVENT_HUB_NAME))
+async def run(args):
+    clients = []
+    for i in range(args.clients):
+        clients.append(EventHubClient.from_connection_string(CONNECTION_STRING, event_hub_path=EVENT_HUB_NAME))
+    client = clients[0]
+    
+    partitionIds = (await client.get_partition_ids())[0:args.partitions]
 
-client = clients[0]
-partitionIds = asyncio.run(client.get_partition_ids())[0:args.partitions]
+    partitions = await asyncio.gather(*[client.get_partition_properties(p) for p in partitionIds])
 
-print(partitionIds)
+    totalCount = 0
+    for partition in partitions:
+        begin = partition["beginning_sequence_number"]
+        end = partition["last_enqueued_sequence_number"]
+        count = end - begin + 1
+        totalCount += count
+
+        if args.verbose:
+            print(f"Partition: {partition['id']}, Begin: {begin}, End: {end}, Count: {count}")
+
+    if args.verbose:
+        print(f"Total Count: {totalCount}")
+
+    # var consumers = new EventHubConsumer[numPartitions];
+    # for (var i = 0; i < numPartitions; i++)
+    # {
+    #     consumers[i] = clients[i % numClients].CreateConsumer(EventHubConsumer.DefaultConsumerGroupName, partitions[i].Id, EventPosition.Earliest);
+    # }
+
+    # try
+    # {
+    #     var receiveTasks = new Task<(int messagesReceived, long lastSequenceNumber)>[numPartitions];
+    #     var sw = Stopwatch.StartNew();
+    #     for (var i = 0; i < numPartitions; i++)
+    #     {
+    #         receiveTasks[i] = ReceiveAllMessages(consumers[i], partitions[i]);
+    #     }
+    #     var results = await Task.WhenAll(receiveTasks);
+    #     sw.Stop();
+
+    #     var elapsed = sw.Elapsed.TotalSeconds;
+    #     var messagesReceived = results.Select(r => r.messagesReceived).Sum();
+    #     var messagesPerSecond = messagesReceived / elapsed;
+    #     var megabytesPerSecond = (messagesPerSecond * _bytesPerMessage) / (1024 * 1024);
+
+    #     Console.WriteLine($"Received {messagesReceived} messages of size {_bytesPerMessage} in {elapsed:N2}s " +
+    #         $"({messagesPerSecond:N2} msg/s, {megabytesPerSecond:N2} MB/s)");
+    # }
+
+
+asyncio.run(run(args))
